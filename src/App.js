1fc9b4c96edd948bf221bc604246b666c4c33617
Sharath <sharath.esr@gmail.com>
2023-05-24 18:49:30 +0530

    feat: tested api calls with mockman

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes,Route } from 'react-router-dom';
 import './App.css';
 import {Home,Products,Wishlist,Cart} from './pages';
 import { Header } from './components/header';
+import Mockman from "mockman-js";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/products" element={<Products/>}/>
           <Route path="/wishlist" element={<Wishlist/>}/>
           <Route path="/cart" element={<Cart/>}/>
+          <Route path="/mockman" element={<Mockman/>}/>
         </Routes> 
     </div>
   );
