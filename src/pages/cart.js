b39aca7f4c86679b5d35b6495d1042afbb967753
Sharath <sharath.esr@gmail.com>
2023-05-28 00:57:31 +0530

    feat: added add to wishlist from cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { useContext } from "react"
 import { DataContext } from ".."
 
@@ -9,13 +10,22 @@ export function Cart(){
             <ul>
                 {
                     state.cart.length===0 ? <p>Cart is empty</p>
-                    : state.cart?.map((product) => <li className="product-item" key={product._id}>
+                    : state.cart?.map((product) =><li className="product-item" key={product._id}>
                         <h4>{product.title}</h4>
                         <img src={product.image} alt={product.title}/>
                         <p>{product.categoryName}</p>
                         <p>{product.price}</p>
-                        <button className="btn-primary">Add to Cart</button>
-                        <button onClick={()=> dispatch({type:'REMOVE_FROM_CART', payload: product._id})}>Remove from Cart</button>
+                        <div className="cart-quantity">
+                          <button className="btn-primary" onClick={()=> dispatch({type:'ADD_TO_CART', payload: product._id})}>+</button>
+                          <p>{(state.cart.filter(item => item===product)).length}</p>
+                          <button className="btn-primary">-</button>
+                        </div>
+                            <button onClick={()=> dispatch({type:'REMOVE_FROM_CART', payload: product._id})}>Remove from Cart</button>
+                        {
+                            state.wishlist.includes(product)
+                            ? <NavLink to="/wishlist" className="nav-link"><button>Go to wishlist</button></NavLink>
+                            : <button onClick={()=> dispatch({type:'ADD_TO_Wishlist', payload: product._id})}>Add to wishlist</button>
+                        }
                     </li>)
                 }
             </ul>
