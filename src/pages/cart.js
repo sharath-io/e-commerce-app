69df4c6441938fe015c9402099b5834f8b0689ab
Sharath <sharath.esr@gmail.com>
2023-05-27 19:36:42 +0530

    feat: added remove from cart functionaliy

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -2,20 +2,20 @@ import { useContext } from "react"
 import { DataContext } from ".."
 
 export function Cart(){
-    const {state} = useContext(DataContext);
+    const {state, dispatch} = useContext(DataContext);
     return (
         <div>
             <h1>Cart page</h1>
             <ul>
                 {
                     state.cart.length===0 ? <p>Cart is empty</p>
-                    : state.cart?.map(({categoryName,title,price,image}) => <li className="product-item">
-                        <h4>{title}</h4>
-                        <img src={image} alt={title}/>
-                        <p>{categoryName}</p>
-                        <p>{price}</p>
+                    : state.cart?.map((product) => <li className="product-item" key={product._id}>
+                        <h4>{product.title}</h4>
+                        <img src={product.image} alt={product.title}/>
+                        <p>{product.categoryName}</p>
+                        <p>{product.price}</p>
                         <button className="btn-primary">Add to Cart</button>
-                        <button>Add to wishlist</button>
+                        <button onClick={()=> dispatch({type:'REMOVE_FROM_CART', payload: product._id})}>Remove from Cart</button>
                     </li>)
                 }
             </ul>
