864e66072846ed0c3da6180c21790627286f409c
Sharath <sharath.esr@gmail.com>
2023-06-05 14:21:08 +0530

    refactor: handled warning 'location' assigned but never used

--- src/contexts/authenticationContext.js
+++ src/contexts/authenticationContext.js
@@ -1,5 +1,5 @@
 import { createContext,useReducer } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import {useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 import { authReducer } from "../reducers/authReducer";
@@ -13,7 +13,7 @@ export function AuthProvider({children}){
         token:''
     }
 
-    const location = useLocation();
+    // const location = useLocation();
     const navigate = useNavigate();
 
 
