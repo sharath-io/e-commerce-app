f565b59b7189920d4a994ea4ec229a536ad3c06e
Sharath <sharath.esr@gmail.com>
2023-06-07 17:09:15 +0530

    fix: fixed search bar only in products page

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 
 import './navbar.css';
 import {DataContext,AuthContext, FilterContext} from '../..';
@@ -8,23 +8,23 @@ export function Navbar(){
     const {state} = useContext(DataContext);
     const {authState} = useContext(AuthContext);
     const {dispatchFilter} = useContext(FilterContext);
+    const localState = useLocation();
     return (
         <div>
             <p className="header">FREE SHIPPING AND RETURNS ON ALL META DEVICES!</p>
             <div className="nav-container">
               <NavLink to="/"  className="nav-link">Meta-Store</NavLink>
-              <label>
+              {localState.pathname==='/products' && <label>
                 <input type="text" placeholder="search product" 
                 onChange={(e)=> dispatchFilter({type:'SEARCH_TEXT', payload: e.target.value})}/>
-              </label>
+              </label>}
               <nav className="nav-bar">
                <NavLink to="/products" className="nav-link">Products </NavLink>
                <NavLink to="/wishlist" className="nav-link">Wishlist ({state.wishlist.length})</NavLink>
                <NavLink to="/cart" className="nav-link">Cart ({state.cart.reduce((totalCart,product) => totalCart+product.qty,0)})</NavLink>
                <NavLink to={authState.isLoggedIn ? "/account-details/userDetails" : "/login"} className="nav-link"> {authState.isLoggedIn ? 'Profile' : 'Login' }</NavLink>
               </nav>
-            </div>
-            
+            </div>           
         </div>
     )
 }
