fdd7b3300bb1ebdbbeaec6b103e2c3afbb7ebfe3
Sharath <sharath.esr@gmail.com>
2023-06-10 03:55:04 +0530

    style: updated styling for search bar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -5,26 +5,31 @@ import './navbar.css';
 import {DataContext,AuthContext, FilterContext} from '../..';
 
 export function Navbar(){
+    const localState = useLocation();
+
     const {state} = useContext(DataContext);
     const {authState} = useContext(AuthContext);
     const {dispatchFilter} = useContext(FilterContext);
-    const localState = useLocation();
+    
+
     return (
         <div>
             <p className="header">FREE SHIPPING AND RETURNS ON ALL META DEVICES!</p>
+
             <div className="nav-container">
               <NavLink to="/"  className="nav-link">Meta-Store</NavLink>
               {localState.pathname==='/products' && <label>
-                <input type="text" placeholder="search product" 
+                <input type="text" placeholder="Search products" className="search-filter"
                 onChange={(e)=> dispatchFilter({type:'SEARCH_TEXT', payload: e.target.value})}/>
               </label>}
+
               <nav className="nav-bar">
                <NavLink to="/products" className="nav-link">Products </NavLink>
                <NavLink to="/wishlist" className="nav-link">Wishlist ({state.wishlist.length})</NavLink>
                <NavLink to="/cart" className="nav-link">Cart ({state.cart.reduce((totalCart,product) => totalCart+product.qty,0)})</NavLink>
                <NavLink to={authState.isLoggedIn ? "/account-details/userDetails" : "/login"} className="nav-link"> {authState.isLoggedIn ? 'Profile' : 'Login' }</NavLink>
               </nav>
-            </div>           
+             </div> 
         </div>
     )
 }
