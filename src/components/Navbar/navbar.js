a6f5996bcda2bc3bd64e35b797a0a4d80aebf590
Sharath <sharath.esr@gmail.com>
2023-06-13 04:01:59 +0530

    style: updated nav bar css

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -3,31 +3,31 @@ import { NavLink, useLocation } from "react-router-dom"
 
 import './navbar.css';
 import {DataContext,AuthContext, FilterContext} from '../..';
+import {getActiveNab} from './getActiveNab';
 
 export function Navbar(){
     const localState = useLocation();
 
     const {state} = useContext(DataContext);
     const {authState} = useContext(AuthContext);
-    const {dispatchFilter} = useContext(FilterContext);
-    
+    const {dispatchFilter} = useContext(FilterContext);  
 
     return (
         <div>
             <p className="header">FREE SHIPPING AND RETURNS ON ALL META DEVICES!</p>
 
             <div className="nav-container">
-              <NavLink to="/"  className="nav-link">Meta-Store</NavLink>
+              <NavLink to="/"  className="nav-link ecom-name">Meta-Store</NavLink>
               {localState.pathname==='/products' && <label>
                 <input type="text" placeholder="Search products" className="search-filter"
                 onChange={(e)=> dispatchFilter({type:'SEARCH_TEXT', payload: e.target.value})}/>
               </label>}
 
               <nav className="nav-bar">
-               <NavLink to="/products" className="nav-link">Products </NavLink>
-               <NavLink to="/wishlist" className="nav-link">Wishlist ({state.wishlist.length})</NavLink>
-               <NavLink to="/cart" className="nav-link">Cart ({state.cart.reduce((totalCart,product) => totalCart+product.qty,0)})</NavLink>
-               <NavLink to={authState.isLoggedIn ? "/account-details/userDetails" : "/login"} className="nav-link"> {authState.isLoggedIn ? 'Profile' : 'Login' }</NavLink>
+               <NavLink to="/products" className="nav-link" style={getActiveNab}>Products </NavLink>
+               <NavLink to="/wishlist" className="nav-link" style={getActiveNab}>{authState.isLoggedIn ?  `Wishlist (${state.wishlist.length})` : 'Wishlist'}</NavLink>
+               <NavLink to="/cart" className="nav-link" style={getActiveNab}>{authState.isLoggedIn ?  `Cart (${state.cart.reduce((totalCart,product) => totalCart+product.qty,0)})` : 'Cart'}</NavLink>
+               <NavLink to={authState.isLoggedIn ? "/account-details/userDetails" : "/login"} className="nav-link" style={getActiveNab}> {authState.isLoggedIn ? 'Profile' : 'Login' }</NavLink>
               </nav>
              </div> 
         </div>
